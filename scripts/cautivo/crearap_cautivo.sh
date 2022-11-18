2d58d41274a0dee15f10fbff7e2a97158da35cd2
javierbu <javierbu@gmail.com>
2022-11-18 18:12:15 +0100

    arreglos en cautivo

--- scripts/cautivo/crearap_cautivo.sh
+++ scripts/cautivo/crearap_cautivo.sh
@@ -9,21 +9,22 @@ else
 	berate_ap --vanilla --no-dnsmasq $ifaceap $ifacenet "${nombreap}" $wpaap 2>&1 >/tmp/berate.log & disown
 	dnsmasq -C /root/scripts/crearap/nat/dnsmasq.conf -q --log-facility=/tmp/dnsmasq3.log -r /root/scripts/crearap/nat/resolv.conf -d >> /tmp/dnsmaq.log & disow
 	sleep 5
+	nombre=$(date +'%A%d-%M-%H-%m')
+        ifacereal=$(ip a | awk -F ":" '{print $2}' | grep ap)
+	if [ -v $ifacereal ];then
+                grep -v GatewayInterface /root/scripts/cautivo/nodogsplash.conf > /root/scripts/cautivo/nds.conf
+                echo -e "GatewayInterface $ifaceap" >> /root/scripts/cautivo/nds.conf
+                nodogsplash -c /root/scripts/cautivo/nds.conf >/tmp/nodog.log & disown
+        else
+                grep -v GatewayInterface /root/scripts/cautivo/nodogsplash.conf > /root/scripts/cautivo/nds.conf
+                echo -e "GatewayInterface $ifacereal" >> /root/scripts/cautivo/nds.conf
+                nodogsplash -c /root/scripts/cautivo/nds.conf >/tmp/nodog.log & disown
+        fi
 	if [ $captura = "si" ];then
-		nombre=$(date +'%A%d-%M-%H-%m')
-		ifacereal=$(ip a | awk -F ":" '{print $2}' | grep ap)
-        	if [ $? = "0" ];then
-		grep -v GatewayInterface /root/scripts/cautivo/nodogsplash.conf > /root/scripts/cautivo/nds.conf
-        	echo -e "GatewayInterface $ifacereal" >> /root/scripts/cautivo/nds.conf
-		tcpdump -i $ifacereal -w /root/scripts/cautivo/capturas/${nombre}.cap & disown
-		nodogsplash -c /root/scripts/cautivo/nds.conf >/tmp/nodog.log & disown
+		if [ -v $ifacereal ];then
+			tcpdump -i $ifaceap -w /root/scripts/cautivo/capturas/${nombre}.cap & disown
 		else
-		grep -v GatewayInterface /root/scripts/cautivo/nodogsplash.conf > /root/scripts/cautivo/nds.conf
-        	echo -e "GatewayInterface $ifaceap" >> /root/scripts/cautivo/nds.conf
-		tcpdump -i $ifaceap -w /root/scripts/cautivo/capturas/${nombre}.cap & disown
-        	nodogsplash -c /root/scripts/cautivo/nds.conf >/tmp/nodog.log & disown
+			tcpdump -i $ifacereal -w /root/scripts/cautivo/capturas/${nombre}.cap & disown
 		fi
 	fi
 fi
-
-
--- scripts/crearap/nat/crearap_nat.sh
+++ scripts/crearap/nat/crearap_nat.sh
@@ -8,7 +8,7 @@ else
 	killall tcpdump 2>/dev/null
 	berate_ap --vanilla --no-dnsmasq $ifaceap $ifacenet "${nombreap}" $wpaap 2>&1 >/tmp/berate.log & disown
 	dnsmasq -C /root/scripts/crearap/nat/dnsmasq.conf -q --log-facility=/tmp/dnsmasq3.log -r /root/scripts/crearap/nat/resolv.conf -d >> /tmp/dnsmaq.log & disow
-	sleep 60
+	sleep 5
 	if [ $captura = "si" ];then
 		nombre=$(date +'%A%d-%M-%H-%m')
 		ifacereal=$(ip a | awk -F ":" '{print $2}' | grep ap)
